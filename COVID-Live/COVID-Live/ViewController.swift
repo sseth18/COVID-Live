4a541f5c4b2124d762acd2d73071278ed65cc3c5
sseth18 <samar.seth@gmail.com>
2020-05-10 19:11:01 -0400

    Initial Commit

---
+++ COVID-Live/COVID-Live/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID-Live
+//
+//  Created by Samar Seth on 5/10/20.
+//  Copyright © 2020 Samar Seth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
